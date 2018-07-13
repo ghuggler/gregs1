72c0c5f1e20e423b5773b27122a175d710fe8564
ghuggler <41211528+ghuggler@users.noreply.github.com>
2018-07-13 16:28:46 -0500

    Add files via upload

---
+++ def procedure example.py
@@ -0,0 +1,9 @@
+# This is a working def procedure
+
+def is_friend (n):
+  return n[0] == 'D'
+# If the first letter of the person's name is 'D' then its a frend
+# returns the result of the boolean test directly
+
+print is_friend('Diane')
+print is_friend('Fred')
